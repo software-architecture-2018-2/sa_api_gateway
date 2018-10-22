845719df81ab0f4270e3e243b1557fa4b3e076ea
Santiago Peña <spenas@MacBook-Pro-de-Santiago.local>
2018-10-21 22:17:43 -0500

    d

--- src/courses/typeDefs.js
+++ src/courses/typeDefs.js
@@ -117,7 +117,7 @@ export const coursesQueries = `
 `;
 
 export const coursesMutations = `
-    createGroup(group: GroupInput!): String!
+    createGroup(group: GroupInput!): Group!
     deleteGroup(code: String!): Int
     updateGroup(code: String!, group: GroupInput!): Group!
 `;
