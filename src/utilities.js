39f760f58cf3652488e9a686c667366fe95346db
Santiago Pena <spenas@unal.edu.co>
2018-11-01 12:47:55 -0500

    CHANHES LOF

--- src/utilities.js
+++ src/utilities.js
@@ -10,6 +10,8 @@ import { formatError } from 'graphql';
  * @return {Promise.<*>} - promise with the error or the response object
  */
 export async function generalRequest(url, method, body, fullResponse) {
+	console.log("============ BODY")
+	console.log(body);
 	const parameters = {
 		method,
 		uri: encodeURI(url),
