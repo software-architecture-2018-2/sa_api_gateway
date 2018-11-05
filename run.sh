c4ef5e8f8978a088c1fbf313d6a732bdd68313c0
Daniel Rodríguez <daerodriguezme@slabcode.com>
2018-11-05 12:54:56 -0500

    Añadido dev_git branch en script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo git pull https://Dandarprox:Veritasium127@github.com/spenas/sapiensbook_api_g.git
+sudo git pull https://Dandarprox:Veritasium127@github.com/spenas/sapiensbook_api_g.git dev_git
 sudo docker-compose down
 sudo docker-compose build
 sudo docker-compose up &
