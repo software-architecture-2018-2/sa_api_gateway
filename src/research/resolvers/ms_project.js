3af48dc0e540737b6a3a0bbf04209a54180a64f3
Santiago Peña <spenas@MacBook-Pro-de-Santiago.local>
2018-11-07 15:57:28 -0500

    changed resolvers projects

--- src/research/resolvers/ms_project.js
+++ src/research/resolvers/ms_project.js
@@ -4,6 +4,7 @@ const ms = 'PROJECT'
 const baseurl = process.env.GENERAL_URL;
 const port = process.env[`${ms}_PORT`];
 const url = `http://${baseurl}:${port}/projects`;
+const url2 = `http://${baseurl}:3001`;
 
 const resolvers = {
     Query: {
@@ -17,14 +18,25 @@ const resolvers = {
   
     Mutation: {
         createProject (_, { project }) {
+          let current_usr = await generalRequestHead(`${url2}/api/users/me`,'GET',token);
+          project.Lider_de_proyecto = current_usr._id;
           return  generalRequest(`${url}`, 'POST', project);
         },
         updateProject (_, { Proyecto_Id, project })
         {
+          let id = await generalRequestHead(`${url2}/api/users/me`, 'GET', token);
+          let cur_proj = await generalRequest(`${url}/${Proyecto_Id}`, 'GET');
+          let members = cur_proj.Miembros
+          if(members.include(id._id)){
            return generalRequest(`${url}/${Proyecto_Id}`, 'PUT', project);
+          }
         },    
         deleteProject (_, { Proyecto_Id }) {
+           let proj = await generalRequest(`${url}/${Proyecto_Id}`, 'GET');
+           let id = await generalRequestHead(`${url2}/api/users/me`, 'GET', token);
+           if(id._id == proj.Lider_de_proyecto){
            return generalRequest(`${url}/${Proyecto_Id}`, 'DELETE');
+           }
         }
     }
   }
